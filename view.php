e6aa50a6fc2f6f6bffab53d47e843e1ca0dec919
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2017-09-21 15:40:09 +0300

    divai

--- view.php
+++ view.php
@@ -4,34 +4,38 @@
 	<title></title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="products.js"></script>
 </head>
 <body>
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
-      <div class="col">			
-        <table class="table">
-  				<thead>
-    				<tr>
-      					<th>ID</th>
-      					<th>Name</th>
-      					<th>Price</th>
-      					<th>Description</th>
-    				</tr>
-  				</thead>
-  				<tbody>
-   					<?php
-   						foreach ($products as $product) {
-						echo "<tr><td>" .$product['id'] . "</td><td>" .$product['name'] . "</td><td class='price'>" .$product['price'] . "</td><td>" . $product['description'] . "</td></tr>";
-						}
-   					?>
-  				</tbody>
-			  </table>
-		  </div>
-      
+      <div class="divoPlotis col">			
+        <h1 class="header">Shop</h1>
+		  </div>  
     </div>
-
 	</div>
+
+      <?php
+        foreach ($products as $product) {
+          echo "<div class='divai'>"
+          ."ID: " .$product['id']
+          ."<br />"  
+          ."Name: " .$product['name']
+          ."<br />"   
+          ."Price: " .$product['price']
+          ."<br />"   
+          ."Description: " .$product['description']
+          ."<br />"
+          ."Image: " ."<img height='50px' src='../images/" .$product['image'] . "'"
+          ."<br />"  
+          ."Weight: ".$product['weight']
+          ."<br />"  
+          ."Qty: " .$product['qty']
+          ."</div>";
+        }
+      ?>
+    
 
 </body>
 </html>
